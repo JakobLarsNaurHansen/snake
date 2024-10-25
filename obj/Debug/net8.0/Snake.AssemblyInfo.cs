d9fbc06771c6e4f64385a6ed21f1b15f660d4134
JakobLarsNaurHansen <s224312@student.dtu.dk>
2024-10-25 12:33:26 +0200

    what is this

--- obj/Debug/net8.0/Snake.AssemblyInfo.cs
+++ obj/Debug/net8.0/Snake.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d3eec2ecde33d7702689240464aa13955774f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd3df386f23e41b1da6bc65b942b963252df73b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
